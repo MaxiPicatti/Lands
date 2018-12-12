10ca2598725952418377cf0098672cbab0dcc45a
MaxiPicatti <45655688+MaxiPicatti@users.noreply.github.com>
2018-12-12 10:23:03 -0300

    LogingPage

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms.Xaml;
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace Lands
+{
+    using Lands.Views;
+    using System;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+
+#region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            //Se ejecuta una sola vez, cuando la app comienza.
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            //Las app no se cierran se duermen, ej en whatsapp cuando te llega un msj
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            //Al abrir whatsapp despues de la llegada de un msj se ejecuta este metodo que te muestra todo como se dejo antes de dormirla
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ Lands/Lands/Infrastructure/IntanceLocator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands.Infrastructure
+{
+    using Lands.ViewModels;
+
+    public class IntanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public IntanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
